973025cfe0698c0ca9ed9b0d593f597661c20d61
moussaba <moussakhady41@gmail.com>
2026-07-01 01:43:44 +0000

    initialisation de 3 employers 2 chefs et 1 non chef

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 
+ function initialisationEmployes(): array {
+    
+      $employes = [
+         0 => [
+            "nom" => "Moussa BA",
+            "matricule" => "MOO1",
+            "departements" => [
+                0 => ["nom" => "depart1" , "code" => "C001"],
+                1 =>["nom" => "depart2" , "code" => "C002"],
+            ],
+            "subordonnes" => [1]
+         ],
+         1 => [
+            "nom" => "Matar BA",
+            "matricule" => "MOO2",
+            "departements" => [
+                0 =>["nom" => "depart3" , "code" => "C001"],
+                1 => ["nom" => "depart2" , "code" => "C002"],
+            ],
+            "subordonnes" => [2]
+         ],
+         2 => [
+            "nom" => "Fatou BA",
+            "matricule" => "MOO3",
+            "departements" => [
+                ["nom" => "depart3" , "code" => "C003"] 
+            ],
+            "subordonnes" => []
+         ]                 
+      ];
+      return $employes;
+  }
 
 
 
